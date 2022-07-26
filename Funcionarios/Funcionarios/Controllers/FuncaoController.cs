fc93b9f36cd0deccacaf4cd6f67f53e8980f6b20
marciomonzon <marcio.pcmonzon@gmail.com>
2022-07-25 21:46:14 -0300

    funcao controller implemented

--- Funcionarios/Funcionarios/Controllers/FuncaoController.cs
+++ Funcionarios/Funcionarios/Controllers/FuncaoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Funcionarios.Domain.DTO;
+using Funcionarios.Domain.Interfaces.Services;
+using Funcionarios.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Funcionarios.Controllers
@@ -7,5 +9,103 @@ namespace Funcionarios.Controllers
     [ApiController]
     public class FuncaoController : ControllerBase
     {
+        private readonly IFuncaoService _funcaoService;
+
+        public FuncaoController(IFuncaoService funcaoService)
+        {
+            _funcaoService = funcaoService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var cargos = await _funcaoService.GetAll();
+
+                if (!cargos.Any()) return NotFound();
+
+                return Ok(cargos);
+            }
+            catch (FuncaoControllerException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var cargo = await _funcaoService.GetById(id);
+
+                if (cargo == null) return NotFound();
+
+                return Ok(cargo);
+            }
+            catch (FuncaoControllerException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCargo([FromBody] FuncaoDTO cargo)
+        {
+            try
+            {
+                if (cargo == null) return NoContent();
+
+                var response = await _funcaoService.Add(cargo);
+
+                if (response.IdRegistro > 0)
+                    return StatusCode(StatusCodes.Status201Created, response.Mensagem);
+
+                return Ok(response);
+            }
+            catch (FuncaoControllerException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCargo(int id, [FromBody] FuncaoDTO cargo)
+        {
+            try
+            {
+                if (cargo == null) return NoContent();
+
+                var response = await _funcaoService.Update(id, cargo);
+
+                return Ok(response);
+            }
+            catch (FuncaoControllerException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCargo(int id)
+        {
+            try
+            {
+                var response = await _funcaoService.Delete(id);
+
+                if (response == null)
+                    return NotFound();
+
+                if (response.IdRegistro == id)
+                    return NoContent();
+
+                return Ok(response);
+            }
+            catch (FuncaoControllerException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
